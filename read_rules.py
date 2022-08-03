2bd676be689a4f6ebfcbb40201ccf71625e3bb6b
zuxinli <lizx21@mails.tsinghua.edu.cn>
2022-08-03 11:43:57 +0800

    Developling: add rules for test, and expand

--- read_rules.py
+++ read_rules.py
@@ -1,6 +1,8 @@
 from logging import warning
 import os
 import re
+import random
+
 
 def file_name(filename):
   return filename + ".file"
@@ -11,12 +13,14 @@ def dup_name(name):
 def read_rules(fh, rules, RE, debug):
   line_num = 0
   while(line_num < len(fh)):
+    print(line_num)
     line = fh[line_num]
+    line_num += 1
     if(line[0] == '#'):
       continue
-    if(len(line) == 0):
+    if(not re.match("\S", line)):
       continue
-    word = line.split(' ')
+    word = line.split()
     name = word[0]
     rule = ""
 
@@ -29,17 +33,18 @@ def read_rules(fh, rules, RE, debug):
       if(file in rules['file']):
         if(debug > 0):
           print ("Skipping duplicate included file $file\n")
-        continue
-    else:
-      rules['file'].append(file)
+          continue
+        else:
+          rules['file'].append(file)
       with open(file, 'r') as f:
         inc_fh = f.readlines()
       read_rules(inc_fh, rules, None, debug)
-
-    if(len(word) == 1 and word[1] == '{'): # name included
+    if(len(word) >= 2 and word[1] == '{'): # name included
       end = 0
       while(line_num < len(fh)):
-        if('}' in line and len(line) == 1):
+        line_num += 1
+        line =fh[line_num]
+        if(re.match("^}[\r\n]+$", line)):
           end = 1
           break
         else:
@@ -47,19 +52,43 @@ def read_rules(fh, rules, RE, debug):
       if(not end):
         print(name, "EOF found before close rule")
     else:
-      rule = line
+      line_rm_trailing = re.match("^\S+\s+", line).group()
+      rule = line_rm_trailing.rstrip('\n')
 
     weight = 1
     name_by_weight = re.match("([^\+]*)\+(\d+)$", name)
     if(name_by_weight):
-      name = name_by_weight.group()
-      weight = name_by_weight.group(1)
+      name = name_by_weight.group(1)
+      weight = eval(name_by_weight.group(2))
       if(debug > 10):
         warning(f"weighting rule by {weight}: {name} {rule}\n")
     
-    do:
+    while(weight > 0):
+      weight = weight - 1
+      if(name == "\n"):
+        print(word)
+      rules['name'].append(name)
+  if(RE is not None):
+    compute_re(rules, RE)
 
-
+def compute_re(rules, RE):
+  in_str = '|'.join(rules['name'].sort(key=lambda x: len(x)))
+  RE = list(f"^(.*?)({in_str})")
+  
+def expand(rules, start, RE, debug):
+  pass
       
+def pop_first_rule(rules, input, RE): # rule and preamble are outputs
+  input_match = re.match(RE, input)
+  if(input_match):
+    preamble = input_match.group(1)
+    rule = input_match.group(2)
+    return preamble, rule
+  return None
 
-      
+def pick_rand(in_set):
+  return in_set[random.randint(0, len(in_set) - 1)]
+
+def generate(rules, start, RE, debug, pretty):
+  s = expand(rules, start, RE, debug)
+  return s
---
+++ unit_test.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append(".")
+
+import read_rules
+
+def test_read_rules(file):
+  with open(file, 'r') as f:
+    fh = f.readlines()
+  rules = {'dup_name': [], 'file': [], 'name': []}
+  RE = None
+  read_rules.read_rules(fh, rules, RE, 10)
+  print(rules)
+  print(RE)
+
+test_read_rules("scirules.in")
